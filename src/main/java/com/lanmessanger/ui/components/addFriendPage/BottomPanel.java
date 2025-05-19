a58b4da2df0ba8f7ef72cd3faaa94f7ba667a867
Shoyeb45 <shoyebff45@gmail.com>
2025-05-19 17:58:17 +0530

    changed the InputField component

--- src/main/java/com/lanmessanger/ui/components/addFriendPage/BottomPanel.java
+++ src/main/java/com/lanmessanger/ui/components/addFriendPage/BottomPanel.java
@@ -1,11 +1,16 @@
 package main.java.com.lanmessanger.ui.components.addFriendPage;
 
-import java.awt.Button;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.plaf.ButtonUI;
+
+import org.kordamp.ikonli.fontawesome.FontAwesome;
+import org.kordamp.ikonli.swing.FontIcon;
+
+import main.java.com.lanmessanger.ui.components.Button;
 
 public class BottomPanel extends JPanel {
     private Button checkConnection;
@@ -16,8 +21,16 @@ public class BottomPanel extends JPanel {
     public BottomPanel() {
         name = new InputField("Name");
         ip = new InputField("IP Address");
+        checkConnection = new Button(null);
+        checkConnection.setText("Connect");
+
+        addFriend = new Button(FontIcon.of(FontAwesome.PLUS, 12));
+        addFriend.setText("Add");
+
         add(name);
         add(ip);
+        add(checkConnection);
+        add(addFriend);
         setBorder(BorderFactory.createLineBorder(Color.yellow));
     }
 }
--- src/main/java/com/lanmessanger/ui/components/addFriendPage/InputField.java
+++ src/main/java/com/lanmessanger/ui/components/addFriendPage/InputField.java
@@ -3,6 +3,7 @@ package main.java.com.lanmessanger.ui.components.addFriendPage;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import java.awt.Dimension;
 
 public class InputField extends JPanel {
     private JLabel label;
@@ -11,7 +12,8 @@ public class InputField extends JPanel {
     public InputField(String text) {
         label = new JLabel(text);
         textField = new JTextField();
-        
+        textField.setSize(100, 20);
+        textField.setPreferredSize(new Dimension(100, 20));
         add(label);
         add(textField);
         
